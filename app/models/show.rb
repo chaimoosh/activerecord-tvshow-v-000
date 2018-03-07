2098eeb51c05dfbbdcc7d47d18166df80037095c
Chaim Reisman <crreisman@gmail.com>
2018-03-07 20:21:27 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -19,7 +19,11 @@ class Show < ActiveRecord::Base
     Show.sum(:rating)
   end
 
-  def popular_shows
-    Show.where(:rating > 5)
+  def self.popular_shows
+    Show.where("rating > 5")
   end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name:)
+  end 
 end
