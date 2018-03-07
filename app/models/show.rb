af2a4e8d96b2edfdd56a6fe16d9efb6dd9a86fa0
Chaim Reisman <crreisman@gmail.com>
2018-03-07 21:13:49 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,11 @@
+require'pry'
 class Show < ActiveRecord::Base
   def self.highest_rating
     Show.maximum(:rating)
   end
 
   def self.most_popular_show
+    binding.pry
     Show.where("Show.highest_rating")
   end
 
